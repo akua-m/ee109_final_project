82ae50a461f4acd77eef1763a6a229ebf4087daf
Angel Luquin <jaluquin@Angels-iMac.local>
2020-05-29 00:28:44 -0700

    test where it's almost all working

--- component_test.scala
+++ component_test.scala
@@ -10,7 +10,7 @@ import spatial.dsl._
     def matrix_mult(a_operand: SRAM2[T], b_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
 
         MemReduce(store_matrix)(a_operand.cols by 1) { c => 
-            val temp = SRAM[T](a_operand.rows, b_operand.cols)
+            val temp = SRAM[T](store_matrix.rows, store_matrix.cols)
 
             Foreach(a_operand.rows.to[Int] by 1) { a =>
                 Foreach(b_operand.cols.to[Int] by 1) { b =>
@@ -49,6 +49,18 @@ import spatial.dsl._
 
     } //end element_add
 
+    def single_element_add(a_operand: SRAM2[T], b_operand: T, store_matrix: SRAM2[T]) : SRAM2[T] = { 
+        
+        Foreach(a_operand.rows.to[Int] by 1) { r =>
+            Foreach(a_operand.cols.to[Int] by 1) { c =>
+                store_matrix(r, c) = a_operand(r, c) + b_operand
+            }
+        }
+
+        store_matrix
+
+    } //end element_add
+
 
     def element_sigmoid(a_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
 
@@ -78,6 +90,27 @@ import spatial.dsl._
         store_matrix
         
     } //end element_tanh
+
+    def store_result (resultant: SRAM2[T], store_matrix: SRAM2[T]) : Unit = {
+        
+        Foreach(resultant.rows.to[Int] by 1) { r =>
+            Foreach(resultant.cols.to[Int] by 1) { c =>
+                store_matrix(r, c) = resultant(r, c)
+            }
+        }
+    }
+
+    def LSTM_Cell (arg_input_gate: SRAM2[T], arg_forget_gate: SRAM2[T], arg_output_gate: SRAM2[T], arg_memory_cell: SRAM2[T], arg_output: SRAM2[T], arg_state: SRAM2[T], state_mem: SRAM2[T], output_mem: SRAM2[T], store_matrix: SRAM2[T]): Unit =  {
+        //state = state * forget_gate + input_gate * memory_cell    
+        //output = output_gate * tf.tanh(state)
+        
+        val state = element_add(element_mult(arg_state, arg_forget_gate, state_mem(0::0, 0::256)), element_mult(arg_input_gate, arg_memory_cell, state_mem(1::1, 0::256)), state_mem(2::2, 0::256))
+        store_result(state, arg_state)
+        
+        val output = element_mult(arg_output_gate, element_tanh(state, store_matrix), output_mem)
+        store_result(output, arg_output)
+
+    } //end LSTM_Cell
 
 
     def tanh(a_operand: T): T = {
@@ -144,45 +177,230 @@ import spatial.dsl._
     
 
     
-    def main (args: Array[String]): Unit = {
+     def main (args: Array[String]): Unit = {
         
         //data processing 
         //prepare memories to pass to Accel
 
         val argRegOut = ArgOut[T]
+    
+        val bias_forget_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/bias_forget_final.csv", ",")
+        val bias_f_mem = DRAM[T](1, 256)
+        setMem(bias_f_mem, bias_forget_array)
+        //println("bias forget array " + bias_forget_array)
+        
+            
+        val bias_input_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/bias_input_final.csv", ",")
+        val bias_in_mem = DRAM[T](1, 256)
+        setMem(bias_in_mem, bias_input_array)
+        //println("bias input array " + bias_input_array)
+        
+        val bias_memory_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/bias_memory_cell_final.csv", ",")
+        val bias_m_mem = DRAM[T](1, 256)
+        setMem(bias_m_mem, bias_memory_array)
+        //println("bias memory array " + bias_memory_array)
+        
+        val bias_output_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/bias_output_final.csv", ",")
+        val bias_o_mem = DRAM[T](1, 256)
+        setMem(bias_o_mem, bias_output_array)
+        //println("bias output array " + bias_output_array)
 
-        val a = DRAM[T](2, 2)
-        val b = DRAM[T](2, 2)
-        val c = DRAM[T](2, 2)
+        val weights_fg_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_forget_gate_final.csv", ",")
+        val weights_fg_mem = DRAM[T](1, 256)
+        setMem(weights_fg_mem, weights_fg_array)
+        //println("weights fg array " + weights_fg_array)
+        
+        val weights_fgh_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_forget_hidden_final.csv", ",")
+        val weights_fgh_mem = DRAM[T](256, 256)
+        setMem(weights_fgh_mem, weights_fgh_array)
+        //println("weights fgh array " + weights_fgh_array)
+        
+        val weights_i_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_input_gate_final.csv", ",")
+        val weights_i_mem = DRAM[T](1, 256)
+        setMem(weights_i_mem, weights_i_array)
+        //println("weights i array " + weights_i_array)
+        
+        val weights_ih_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_input_hidden_final.csv", ",")
+        val weights_ih_mem = DRAM[T](256, 256)
+        setMem(weights_ih_mem, weights_ih_array)
+        //println("weights ih array " + weights_ih_array)
+        
+        val weights_m_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_memory_cell_final.csv", ",")
+        val weights_m_mem = DRAM[T](1, 256)
+        setMem(weights_m_mem, weights_m_array)
+        //println("weights m array " + weights_m_array)
+        
+        val weights_mh_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_memory_cell_hidden_final.csv", ",")
+        val weights_mh_mem = DRAM[T](256, 256)
+        setMem(weights_mh_mem, weights_mh_array)
+        //println("weights mh array " + weights_mh_array)
+        
+        val weights_o_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_output_final.csv", ",")
+        val weights_o_mem = DRAM[T](256, 1)
+        setMem(weights_o_mem, weights_o_array)
+       //println("weights o array " + weights_o_array)
+        
+        val weights_og_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_output_gate_final.csv", ",")
+        val weights_og_mem = DRAM[T](1, 256)
+        setMem(weights_og_mem, weights_og_array)
+        //println("weights og array " + weights_og_array)
+        
+        val weights_oh_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/weights_output_hidden_final.csv", ",")
+        val weights_oh_mem = DRAM[T](256, 256)
+        setMem(weights_oh_mem, weights_oh_array)
+        //println("weights oh array " + weights_oh_array)
+
+        val window_array = loadCSV1D[T]("/home/akuam/spatial/apps/src/window.csv", ",")
+        val window_mem = DRAM[T](7)
+        setMem(window_mem, window_array)
+        //println("window_array " + window_array)
+
+        val zero_init = (0::256){(j) => 0.to[T] }
+        println("zero_init " + zero_init)
+        val zeros = DRAM[T](1, 256)
+        setMem(zeros, zero_init)
+
+        //val zero_init_2 = (0::0, 0::256){(i,j) => 0.to[T] }
+        //println("zero_init " + zero_init)
+        //val zeros_2 = DRAM[T](1, 256)
+        //setMem(zeros_2, zero_init_2)
+
+        val result_one = DRAM[T](1, 256)
+        val result_two = DRAM[T](1, 256)
+        val result_three = DRAM[T](1, 256)
+        val result_four = DRAM[T](1, 256)
+
+        val state_dram = DRAM[T](1, 256)
+        val output_dram = DRAM[T](1, 256)
+        val prediction_dram = DRAM[T](1, 1)
+
+        val bias_output_dram = DRAM[T](1,1)
 
         Accel { 
 
-            val a_sram = SRAM[T](2, 2)
-            a_sram(0,0) = 1
-            a_sram(0,1) = 1
-            a_sram(1,0) = 1
-            a_sram(1,1) = 1
+            val bias_output_layer = SRAM[T](1, 1)
+            bias_output_layer(0,0) = 0.10646543651819229125976562500000.to[T]
 
-            val b_sram = SRAM[T](2, 2)
-            b_sram(0,0) = 2
-            b_sram(0,1) = 2
-            b_sram(1,0) = 3
-            b_sram(1,1) = 4
+            //set memories
+            val bias_forget = SRAM[T](1, 256)
+            bias_forget load bias_f_mem
 
-            val store = SRAM[T](2, 2)
-            //val resulta = element_mult(a_sram, b_sram, store)
-            //val resultb = element_add(a_sram, b_sram, store)
-            val resultc = matrix_mult(a_sram, b_sram, store)
+            val bias_input = SRAM[T](1, 256)
+            bias_input load bias_in_mem
 
-            a store resulta
-            b store resultb
-            c store resultc
+            val bias_memory_cell = SRAM[T](1, 256)
+            bias_memory_cell load bias_m_mem
 
+            val bias_output = SRAM[T](1, 256)
+            bias_output load bias_o_mem
+
+            val weights_forget_gate = SRAM[T](1, 256)
+            weights_forget_gate load weights_fg_mem
+            
+            val weights_forget_hidden = SRAM[T](256, 256)
+            weights_forget_hidden load weights_fgh_mem
+
+            val weights_input_gate = SRAM[T](1, 256)
+            weights_input_gate load weights_i_mem
+
+            val weights_input_hidden = SRAM[T](256, 256)
+            weights_input_hidden load weights_ih_mem
+
+            val weights_memory_cell = SRAM[T](1, 256)
+            weights_memory_cell load weights_m_mem
+
+            val weights_memory_cell_hidden = SRAM[T](256, 256)
+            weights_memory_cell_hidden load weights_mh_mem
+
+            val weights_output = SRAM[T](256, 1)
+            weights_output load weights_o_mem
+
+            val weights_output_gate = SRAM[T](1, 256)
+            weights_output_gate load weights_og_mem
+
+            val weights_output_hidden = SRAM[T](256, 256)
+            weights_output_hidden load weights_oh_mem
+
+            val window = SRAM[T](7)
+            window load window_mem
+        
+
+            //for loop to scroll through input window
+
+            val state = SRAM[T](1, 256)
+            state load zeros
+            val output = SRAM[T](1, 256)
+            output load zeros
+
+            val input = SRAM[T](1, 1)
+
+
+            val input_sram = SRAM[T](4, 256)
+            val forget_sram = SRAM[T](4, 256)
+            val output_sram = SRAM[T](4, 256)
+            val memory_sram = SRAM[T](4, 256)
+
+            val activation_sram = SRAM[T](5, 256)
+
+            val state_sram = SRAM[T](3, 256)
+            val output_lstm_sram = SRAM[T](1, 256)
+
+
+            input(0, 0) = window(0)
+
+            val memory_cell_matrix = element_add(element_add(matrix_mult(input, weights_memory_cell, memory_sram(0::0, 0::256)), matrix_mult(output, weights_memory_cell_hidden, output_sram(1::1, 0::256)), memory_sram(2::2, 0::256)), bias_memory_cell, memory_sram(3::3, 0::256))
+            val memory_cell = element_tanh(memory_cell_matrix, activation_sram(3::3, 0::256))
+
+            result_one store memory_cell
+
+            val input_gate_matrix = element_add(element_add(matrix_mult(input, weights_input_gate, input_sram(0::0, 0::256)), matrix_mult(output, weights_input_hidden, input_sram(1::1, 0::256)), input_sram(2::2, 0::256)), bias_input, input_sram(3::3, 0::256))        
+            val input_gate = element_sigmoid(input_gate_matrix, activation_sram(0::0, 0::256))
+
+            result_two store input_gate
+      
+            val forget_gate_matrix= element_add(element_add(matrix_mult(input, weights_forget_gate, forget_sram(0::0, 0::256)), matrix_mult(output, weights_forget_hidden, forget_sram(1::1, 0::256)), forget_sram(2::2, 0::256)), bias_forget, forget_sram(3::3, 0::256))
+            val forget_gate = element_sigmoid(forget_gate_matrix, activation_sram(1::1, 0::256))
+
+            result_three store forget_gate
+                
+            val output_gate_matrix = element_add(element_add(matrix_mult(input, weights_output_gate, output_sram(0::0, 0::256)), matrix_mult(output, weights_output_hidden, output_sram(1::1, 0::256)), output_sram(2::2, 0::256)), bias_output, output_sram(3::3, 0::256))
+            val output_gate = element_sigmoid(output_gate_matrix, activation_sram(2::2, 0::256))
+
+            result_four store output_gate
+
+            LSTM_Cell(input_gate, forget_gate, output_gate, memory_cell, state, output, state_sram, output_lstm_sram,activation_sram(4::4, 0::256))
+
+
+            state_dram store state
+            output_dram store output
+
+            bias_output_dram store bias_output_layer
+
+            val prediction_sram = SRAM[T](2, 1)
+            val prediction = single_element_add(matrix_mult(output, weights_output, prediction_sram(0::0, 0::0)), bias_output_layer(0,0), prediction_sram(1::1, 0::0))
+
+            prediction_dram store prediction
+
+            argRegOut := prediction(0,0)
+            
         }
-
+        
         //printMatrix(getMatrix(a), "element mult: ")
         //printMatrix(getMatrix(b), "element add: ")
-        printMatrix(getMatrix(c), "matrix mult: ")
+        printMatrix(getMatrix(result_one), "result_one: ")
+        printMatrix(getMatrix(result_two), "result_two: ")
+        printMatrix(getMatrix(result_three), "result_three: ")
+        printMatrix(getMatrix(result_four), "result_four: ")
+
+        printMatrix(getMatrix(bias_output_dram), "bias: ")
+
+        printMatrix(getMatrix(state_dram), "state: ")
+        printMatrix(getMatrix(output_dram), "output: ")
+
+
+        printMatrix(getMatrix(prediction_dram), "prediction: ")
+
+        println("prediction: " + argRegOut.value)
 
     }
 }
