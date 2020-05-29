17d2bfc0b7897772d8ed71709ebbb83338387dac
Angel Luquin <jaluquin@Angels-iMac.local>
2020-05-28 17:47:31 -0700

    sigmoid function

--- fixed_memory_issues.scala
+++ fixed_memory_issues.scala
@@ -6,23 +6,23 @@ import spatial.dsl._
     // type T = Float  //Float = FltPt[_24, _8] = [_significant bits, _exponent bits]
     type T = FixPt[TRUE, _16, _16]
 
+
+    /*
     def sigmoid(a: T) = { //: T = {
       // TODO: Fill in your sigmoid implementation.
       a
-      //val a_sigmoid_test = Reg[T](0)
-      //a_sigmoid_test := (tanh_test(a/2) + 1)/2
-
 
     }
+    */
 
+    /*
     def tanh(a: T) = {
       // TODO: Fill in your tanh implementation.
       a
     }
+    */
     
     def matrix_mult(a_operand: SRAM2[T], b_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
-
-        //val c_sram = SRAM[T](a_operand.rows.to[Int], b_operand.cols.to[Int])
 
         MemReduce(store_matrix)(a_operand.cols by 1) { c => 
             val temp = SRAM[T](a_operand.rows, b_operand.cols)
@@ -36,12 +36,10 @@ import spatial.dsl._
             temp
         }{_+_}
 
-    }
+    } //end matrix_mult
 
 
     def element_mult(a_operand: SRAM2[T], b_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = {  
-
-        //val c_sram = SRAM[T](a_operand.rows, a_operand.cols)  //a_operand and b_operand are the same size
         
         Foreach(a_operand.rows.to[Int] by 1) { r =>
             Foreach(a_operand.cols.to[Int] by 1) { c =>
@@ -50,11 +48,11 @@ import spatial.dsl._
         }
 
         store_matrix
-    }
+
+    } //end element_mult
+
 
     def element_add(a_operand: SRAM2[T], b_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
-
-        //val c_sram = SRAM[T](a_operand.rows, a_operand.cols)  //a_operand and b_operand are the same size
         
         Foreach(a_operand.rows.to[Int] by 1) { r =>
             Foreach(a_operand.cols.to[Int] by 1) { c =>
@@ -63,39 +61,39 @@ import spatial.dsl._
         }
 
         store_matrix
-    }
+
+    } //end element_add
+
 
     def element_sigmoid(a_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
 
-    	/*
-        //val a_sigmoid = SRAM[T](a_operand.rows, a_operand.cols)  //a_operand and b_operand are the same size
-        
+        //sigmoid formula: sigmoid(x) = (tanh(x/2) + 1) / 2
+
         Foreach(a_operand.rows.to[Int] by 1) { r =>
             Foreach(a_operand.cols.to[Int] by 1) { c =>
-                store_matrix(r, c) = sigmoid(a_operand(r, c))
+                val tanh_value = tanh((a_operand(r, c)) / 2)
+                val sigmoid = ((tanh_value + 1) / 2)
+                store_matrix(r, c) = sigmoid
             }
         }
-        */
-
-        a_operand
-
-    }
-
-    def element_tanh(a_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
-
-    	
-        //val a_tanh = SRAM[T](a_operand.rows, a_operand.cols)  //a_operand and b_operand are the same size
-        
-        Foreach(a_operand.rows.to[Int] by 1) { r =>
-            Foreach(a_operand.cols.to[Int] by 1) { c =>
-                store_matrix(r, c) = tanh_test(a_operand(r, c))
-            }
-        }
-        
 
         store_matrix
+
+    } //end element_sigmoid
+
+
+    def element_tanh(a_operand: SRAM2[T], store_matrix: SRAM2[T]) : SRAM2[T] = { 
         
-    }
+        Foreach(a_operand.rows.to[Int] by 1) { r =>
+            Foreach(a_operand.cols.to[Int] by 1) { c =>
+                store_matrix(r, c) = tanh(a_operand(r, c))
+            }
+        }
+        
+        store_matrix
+        
+    } //end element_tanh
+
 
     def LSTM_Cell (arg_input_gate: SRAM2[T], arg_forget_gate: SRAM2[T], arg_output_gate: SRAM2[T], arg_memory_cell: SRAM2[T], arg_output: SRAM2[T], arg_state: SRAM2[T], state_mem: SRAM2[T], output_mem: SRAM2[T], store_matrix: SRAM2[T]): Unit =  {
         //state = state * forget_gate + input_gate * memory_cell    
@@ -105,80 +103,77 @@ import spatial.dsl._
 
         val output = element_mult(arg_output_gate, element_tanh(state, store_matrix), output_mem)
 
-    }
+    } //end LSTM_Cell
 
-    
 
-    def tanh_test(a_operand: T): T = {
+    def tanh(a_operand: T): T = {
         val sigmoid_lut = LUT[T](15,3)(0.390625.to[T], 0.453125.to[T], 0.4049.to[T], 0.453125.to[T], 0.515625.to[T], 0.4558.to[T], 0.515625.to[T], 0.578125.to[T], 0.5038.to[T], 0.578125.to[T], 0.640625.to[T], 0.5490.to[T], 0.640625.to[T], 0.703125.to[T], 0.5911.to[T], 0.703125.to[T], 0.78125.to[T], 0.6348.to[T], 0.78125.to[T], 0.859375.to[T], 0.6791.to[T], 0.859375.to[T], 0.9375.to[T], 0.7190.to[T], 0.9375.to[T], 1.046875.to[T], 0.7609.to[T], 1.046875.to[T], 1.171875.to[T], 0.8057.to[T], 1.171875.to[T], 1.328125.to[T], 0.8493.to[T], 1.328125.to[T], 1.53125.to[T], 0.8916.to[T], 1.53125.to[T], 1.859375.to[T], 0.9329.to[T], 1.859375.to[T], 2.90625.to[T], 0.9740.to[T], 2.90625.to[T], 2.90625.to[T], 1.to[T])
-        val a_tanh_test = Reg[T](0)
+        val a_tanh = Reg[T](0)
+
         val temp = Reg[T](0)
         val temp2 = Reg[T](0)
 
-        //Foreach(a_operand.rows.to[Int] by 1) { r =>
-            //Foreach(a_operand.cols.to[Int] by 1) { c =>
+        Sequential { 
 
-            Sequential { 
-
-                if (a_operand < 0) {
-                    temp := -1 * a_operand
-                } else {
-                    temp := a_operand
-                }
+            if (a_operand < 0) {
+                temp := -1 * a_operand
+            } else {
+                temp := a_operand
+            }
                 
 
-                if (sigmoid_lut(0,0) < temp && temp <= sigmoid_lut(0,1)) {
-                    temp2 := sigmoid_lut(0,2) 
-                } else if (sigmoid_lut(1,0) < temp && temp <= sigmoid_lut(1,1)) {
-                    temp2 := sigmoid_lut(1,2) 
-                } else if (sigmoid_lut(2,0) < temp && temp <= sigmoid_lut(2,1)) {
-                    temp2 := sigmoid_lut(2,2)
-                } else if (sigmoid_lut(3,0) < temp && temp <= sigmoid_lut(3,1)) {
-                    temp2 := sigmoid_lut(3,2)
-                } else if (sigmoid_lut(4,0) < temp && temp <= sigmoid_lut(4,1)) {
-                    temp2 := sigmoid_lut(4,2)
-                } else if (sigmoid_lut(5,0) < temp && temp <= sigmoid_lut(5,1)) {
-                    temp2 := sigmoid_lut(5,2)
-                } else if (sigmoid_lut(6,0) < temp && temp <= sigmoid_lut(6,1)) {
-                    temp2 := sigmoid_lut(6,2)
-                } else if (sigmoid_lut(7,0) < temp && temp <= sigmoid_lut(7,1)) {
-                    temp2 := sigmoid_lut(7,2)
-                } else if (sigmoid_lut(8,0) < temp && temp <= sigmoid_lut(8,1)) {
-                    temp2 := sigmoid_lut(8,2)
-                } else if (sigmoid_lut(9,0) < temp && temp <= sigmoid_lut(9,1)) {
-                    temp2 := sigmoid_lut(9,2)
-                } else if (sigmoid_lut(10,0) < temp && temp <= sigmoid_lut(10,1)) {
-                    temp2 := sigmoid_lut(10,2)
-                } else if (sigmoid_lut(11,0) < temp && temp<= sigmoid_lut(11,1)) {
-                    temp2 := sigmoid_lut(11,2)
-                } else if (sigmoid_lut(12,0) < temp && temp<= sigmoid_lut(12,1)) {
-                    temp2 := sigmoid_lut(12,2)
-                } else if (sigmoid_lut(13,0) < temp && temp <= sigmoid_lut(13,1)) {
-                    temp2 := sigmoid_lut(13,2)
-                } else {
-                    temp2 := sigmoid_lut(14,2)
-                }
-                
-
-                if (a_operand < 0) {
-                    a_tanh_test := 1 - temp2.value
-                } else {
-                    a_tanh_test := temp2.value
-                }
+            if (sigmoid_lut(0,0) < temp && temp <= sigmoid_lut(0,1)) {
+                temp2 := sigmoid_lut(0,2) 
+            } else if (sigmoid_lut(1,0) < temp && temp <= sigmoid_lut(1,1)) {
+                temp2 := sigmoid_lut(1,2) 
+            } else if (sigmoid_lut(2,0) < temp && temp <= sigmoid_lut(2,1)) {
+                temp2 := sigmoid_lut(2,2)
+            } else if (sigmoid_lut(3,0) < temp && temp <= sigmoid_lut(3,1)) {
+                temp2 := sigmoid_lut(3,2)
+            } else if (sigmoid_lut(4,0) < temp && temp <= sigmoid_lut(4,1)) {
+                temp2 := sigmoid_lut(4,2)
+            } else if (sigmoid_lut(5,0) < temp && temp <= sigmoid_lut(5,1)) {
+                temp2 := sigmoid_lut(5,2)
+            } else if (sigmoid_lut(6,0) < temp && temp <= sigmoid_lut(6,1)) {
+                temp2 := sigmoid_lut(6,2)
+            } else if (sigmoid_lut(7,0) < temp && temp <= sigmoid_lut(7,1)) {
+                temp2 := sigmoid_lut(7,2)
+            } else if (sigmoid_lut(8,0) < temp && temp <= sigmoid_lut(8,1)) {
+                temp2 := sigmoid_lut(8,2)
+            } else if (sigmoid_lut(9,0) < temp && temp <= sigmoid_lut(9,1)) {
+                temp2 := sigmoid_lut(9,2)
+            } else if (sigmoid_lut(10,0) < temp && temp <= sigmoid_lut(10,1)) {
+                temp2 := sigmoid_lut(10,2)
+            } else if (sigmoid_lut(11,0) < temp && temp<= sigmoid_lut(11,1)) {
+                temp2 := sigmoid_lut(11,2)
+            } else if (sigmoid_lut(12,0) < temp && temp<= sigmoid_lut(12,1)) {
+                temp2 := sigmoid_lut(12,2)
+            } else if (sigmoid_lut(13,0) < temp && temp <= sigmoid_lut(13,1)) {
+                temp2 := sigmoid_lut(13,2)
+            } else {
+                temp2 := sigmoid_lut(14,2)
+            }
             
-        	}
 
-        a_tanh_test.value        
-    }
+            if (a_operand < 0) {
+                a_tanh := 1 - temp2.value
+            } else {
+                a_tanh := temp2.value
+            }
+        
+    	} //end Sequential
+
+
+        a_tanh.value  
+
+    } //end tanh
     
 
     
     def main (args: Array[String]): Unit = {
+        
         //data processing 
         //prepare memories to pass to Accel
-        
-        //val file_loc = "/home/jluquin/spatial/apps/src/"
-        //val bias_forget_file = io.Source.fromFile(file_loc + "bias_forget.csv")
 
         val argRegOut = ArgOut[T]
     
@@ -239,10 +234,9 @@ import spatial.dsl._
         val window_mem = DRAM[T](7)
         setMem(window_mem, window_array)
 
-        //val initialize_data = Array.tabulate[T](256){i => 0.to[T]}
-        val c_init = (0::0, 0::256){(i,j) => 0.to[T] }
-        val d1 = DRAM[T](1, 256)
-        setMem(d1, c_init)
+        val zero_init = (0::0, 0::256){(i,j) => 0.to[T] }
+        val zeros = DRAM[T](1, 256)
+        setMem(zeros, zero_init)
 
         val v = ArgIn[T]
         setArg(v, args(0).to[T])
@@ -250,12 +244,7 @@ import spatial.dsl._
         Accel { 
 
             val bias_output_layer = SRAM[T](1, 1)
-            //val input = args(0.10646543651819229125976562500000).to[Float]
             bias_output_layer(0,0) = 0.10646543651819229125976562500000.to[T]
-            //val bias_olayer_array = loadCSV1D[T](s"jluquin@tucson.stanford.edu:/home/jluquin/spatial/apps/src/bias_output_layer_final.csv", " ")
-            //val bias_ol_mem = DRAM[T](1)
-            //setMem(bias_ol_mem, bias_olayer_array)
-            //bias_output_layer load input
 
             //set memories
             val bias_forget = SRAM[T](1, 256)
@@ -305,14 +294,12 @@ import spatial.dsl._
 
             val state = SRAM[T](1, 256)
             val output = SRAM[T](1, 256)
-            output load d1
+            output load zeros
 
             val input = SRAM[T](1, 1)
             input(0,0) = v.value
 
 
-            //val len = 1
-            //val vec1 = Array.tabulate[T](len){i => i.to[T]}
             val input_sram = SRAM[T](4, 256)
             val forget_sram = SRAM[T](4, 256)
             val output_sram = SRAM[T](4, 256)
